9470ba258eaaf42f2dcae0efb1178cedf48d2f37
Kamen Stanchev <k.s@abv.bg>
2022-05-29 16:52:00 +0300

    classes

--- Petstagram/main_app/models.py
+++ Petstagram/main_app/models.py
@@ -3,22 +3,8 @@ from django.db import models
 
 from Petstagram.main_app.validator import only_letters_validator
 
-""""Profile
-The user must provide the following information in their profile:
-•	The first name - it should have at least 2 chars, max - 30 chars, and should consist only of letters.
-•	The last name - it should have at least 2 chars, max - 30 chars, and should consist only of letters.
-•	Profile picture - the user can link their picture using a URL.
-
-The user may provide the following information in their profile:
-•	Date of birth: day, month, and year of birth.
-•	Description - a user can write any description about themselves, no limit of words/chars.
-•	Email - a user can only write a valid email address.
-•	Gender - the user can choose one of the following: "Male", "Female", and "Do not show".
-"""
-
 
 class Profile(models.Model):
-
     first_name = models.CharField(
         max_length=30,
         validators=(
@@ -34,7 +20,10 @@ class Profile(models.Model):
         )
     )
 
-    picture = models.URLField()
+    picture = models.URLField(
+        null=True,
+        blank=True
+    )
     date_of_birth = models.DateField(
         null=True,
         blank=True
@@ -53,6 +42,36 @@ class Profile(models.Model):
         null=True,
         blank=True
     )
+    def __str__(self):
+        return self.first_name
 
 
+# Pet
+# The user must provide the following information when adding a pet in their profile:
+# •	Name - it should consist of maximum 30 characters. All pets' names should be unique for that user.
+# •	Type - the user can choose one of the following: "Cat", "Dog", "Bunny", "Parrot", "Fish", or "Other".
+# The user may provide the following information when adding a pet to their profile:
+# •	Date of birth - pet's day, month, and year of birth.
 
+
+class Pet(models.Model):
+    TYPES = [(x, x) for x in ("Cat", "Dog", "Bunny", "Parrot", "Fish", "Other")]
+    name = models.CharField(max_length=30)
+    type = models.CharField(
+        max_length=max(len(x) for (x, y) in TYPES),
+        choices=TYPES
+    )
+    date_of_birth = models.DateField(
+        null=True,
+        blank=True
+    )
+    user_profile = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        unique_together = ('user_profile', 'name')
+
+    def __str__(self):
+        return self.name
