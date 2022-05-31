dc1a51b080c67880bea952fc5300b233eaf5a2a1
Kamen Stanchev <k.s@abv.bg>
2022-05-31 09:47:07 +0300

    31.05

--- Petstagram/main_app/views.py
+++ Petstagram/main_app/views.py
@@ -1,12 +1,29 @@
 from django.shortcuts import render
 
+from Petstagram.main_app.models import Profile
+
+
+def get_profile():
+    profiles = Profile.objects.all()
+    if profiles:
+        return profiles[0]
+    return None
+
 
 def home_page(request):
-    return render(request, 'home_page.html')
+    context = {
+        'hide_additional_nav_item': True,
+    }
+    return render(request, 'home_page.html', context)
 
 
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    profile = get_profile()
+    pets = profile.pet_set.all()
+    context = {
+        'pets': pets,
+    }
+    return render(request, 'dashboard.html', context)
 
 
 def photo_details(request, pk):
